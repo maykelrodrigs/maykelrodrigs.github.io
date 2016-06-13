b695f147c7f6946a59051ee168576180502ba14c
Maykel Rodrigues <maykelrodrigues@live.com>
2016-06-13 13:47:40 -0300

    Inserido menu toggle

--- js/custom.js
+++ js/custom.js
@@ -61,3 +61,53 @@ $(function() {
         $('body,html').animate({scrollTop:0},800);
     });
 });
+
+
+$(document).ready(function(){
+var myNavBar = {
+
+    flagAdd: true,
+    elements: [],
+    init: function (elements) {
+        this.elements = elements;
+    },
+    add : function() {
+        if(this.flagAdd) {
+            for(var i=0; i < this.elements.length; i++) {
+                document.getElementById(this.elements[i]).className += " fixed-theme";
+            }
+            this.flagAdd = false;
+        }
+    },
+    remove: function() {
+        for(var i=0; i < this.elements.length; i++) {
+            document.getElementById(this.elements[i]).className =
+                    document.getElementById(this.elements[i]).className.replace( /(?:^|\s)fixed-theme(?!\S)/g , '' );
+        }
+        this.flagAdd = true;
+    }
+};
+
+myNavBar.init(  [
+    "header",
+    "header-container",
+    "brand"
+]);
+function offSetManager(){
+
+    var yOffset = 500;
+    var currYOffSet = window.pageYOffset;
+
+    if(yOffset < currYOffSet) {
+        myNavBar.add();
+    }
+    else if(currYOffSet < yOffset){
+        myNavBar.remove();
+    }
+
+}
+window.onscroll = function(e) {
+    offSetManager();
+}
+offSetManager();
+});
