90f405b308361841eed0ac09e7d5a2e658339508
Maykel Rodrigues <maykelrodrigues@live.com>
2016-06-13 10:40:34 -0300

    Botão back to top inserido

--- js/custom.js
+++ js/custom.js
@@ -39,3 +39,17 @@ $(window).scroll(function(){
     .end().filter("[href=#"+id+"]").parent().addClass("active");
   }
 });
+
+$(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+            $('#backtotop').fadeIn();
+        } else {
+            $('#backtotop').fadeOut();
+        }
+    });
+
+    $('#backtotop').click(function() {
+        $('body,html').animate({scrollTop:0},800);
+    });
+});
